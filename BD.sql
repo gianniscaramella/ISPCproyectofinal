bcd55282038db649d8969b032a8ff1db3a9056d2
lautaross11 <135083264+lautaross11@users.noreply.github.com>
2023-06-22 03:06:45 -0300

    Update BD.sql
    Cambio de numero máximo de caracteres permitidos en las columnas 10 apellidos, 12 descripción, 13 dirección, 14 categoría, 15 jurisdicción.
    Se agrego la propiedad not null en las columnas 12 descripción y 13 dirección

--- BD.sql
+++ BD.sql
@@ -7,12 +7,12 @@ CREATE TABLE Leyes
 Numero_Registro int primary key not null,
 Numero_Normativa int not null,
 Tipo_Normativa varchar (10) not null,
-Apellidos varchar (35) not null,
+Apellidos varchar (10) not null,
 Fecha int not null,
-Descripcion varchar (40),
-Direccion varchar (50),
-Categoria varchar (35) not null,
-Jurrisdiccion varchar (35) not null,
+Descripcion varchar (20) not null,
+Direccion varchar (20) not null,
+Categoria varchar (25) not null,
+Jurrisdiccion varchar (20) not null,
 Organo_Legislativo varchar (35) not null,
 Palabra_clave varchar (35) not null
 
