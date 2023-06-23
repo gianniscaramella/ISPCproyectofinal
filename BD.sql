abe670f0be246849c5cb36a4113979dc587aeda4
Joaquin Sosa <135076382+Joacososa22@users.noreply.github.com>
2023-06-23 03:37:30 -0300

    Base datos

---
+++ BD.sql
@@ -0,0 +1,18 @@
+
+use BaseDatos;
+
+CREATE TABLE Leyes
+
+(
+Numero_Registro int primary key not null,
+Numero_Normativa int not null,
+Tipo_Normativa varchar (12) not null,
+Fecha int not null,
+Descripcion varchar (320) not null,
+Categoria varchar (30) not null,
+Jurisdiccion varchar (10) not null,
+Organo_Legislativo varchar (35) not null,
+Palabra_clave varchar (65) not null
+
+);
+
