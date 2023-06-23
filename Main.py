f47bf66dd55ada9acb0304d7bd8efc83674cc03f
Kiara Nakayama <134019236+221004@users.noreply.github.com>
2023-06-23 19:57:05 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -6,16 +6,16 @@ def Menu():
     while(continuar):
         opcionCorrecta = False
         while(not opcionCorrecta):
-            print("Seleccione una opcion")
-            print("1.- Listar leyes")
-            print("2.- Registrar ley")
-            print("3.- Actualizar ley")
-            print("4.- Eliminar ley")
+            print("Seleccione una opcion ")
+            print("1.- Listar leyes ")
+            print("2.- Registrar ley ")
+            print("3.- Actualizar ley ")
+            print("4.- Eliminar ley ")
             print("5.- Salir\n")
             opcion = int(input("Seleccione una opción: "))
         
             if opcion < 1 or opcion > 5:
-                print("\nOpción erronea, ingrese de nuevo.\n")
+                print("\n Opción erronea, ingrese de nuevo.\n")
             elif opcion == 5:
              continuar = False
              print("¡Gracias por usar este sistema!")
